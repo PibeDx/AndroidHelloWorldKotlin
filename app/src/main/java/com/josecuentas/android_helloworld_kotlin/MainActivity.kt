bbd0aa5831802100f67c8b409f5de6b7ca259a8f
José Cuentas Turpo <jcuentast@gmail.com>
2017-05-26 17:05:50 -0500

    feat: add fun hello

--- app/src/main/java/com/josecuentas/android_helloworld_kotlin/MainActivity.kt
+++ app/src/main/java/com/josecuentas/android_helloworld_kotlin/MainActivity.kt
@@ -2,11 +2,27 @@ package com.josecuentas.android_helloworld_kotlin
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.text.Html
+import android.text.Spanned
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        events()
     }
+
+    private fun events() {
+        butIngresar.setOnClickListener {
+            var name = eteName.text
+            var cadena = getString(R.string.main_hi, name)
+            val fromHtml: Spanned = Html.fromHtml(cadena)
+            tviMessage.text = fromHtml
+        }
+    }
+
 }
